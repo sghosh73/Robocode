b9c20667be5044521e28ac3f0624d405f9370e0e
Sayan Ghosh <sggsayan93@gmail.com>
2014-10-31 22:09:39 -0700

    BASF skeleton code

---
+++ BayAreaScienceFestival.c
@@ -0,0 +1,50 @@
+
+//includes code for mecanum drive
+#include "Drive.c"
+
+task main()
+{
+
+	while (true) {
+
+		//spins spinner outward
+		if (joy1Btn(5)) {
+
+		}
+
+		//spins spinner
+		if (joy1Btn(7)) {
+
+		}
+
+		//lowest lift height
+		if (joy2Btn(1)) {
+			
+		}
+
+		//lift = 30
+		if (joy2Btn(2)) {
+			
+		}
+
+		//lift = 60
+		if (joy2Btn(3)) {
+			
+		}
+
+		//lift = 90
+		if (joy2Btn(4)) {
+			
+		}
+
+		//servo door
+		if (joy2Btn(6)) {
+			
+		}
+
+		//max lift height
+		if (joy2Btn(8)) {
+			
+		}
+	}
+}
